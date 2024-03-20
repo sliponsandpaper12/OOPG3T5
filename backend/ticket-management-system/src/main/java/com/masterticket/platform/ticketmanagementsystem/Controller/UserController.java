77a47b950ef633f46984dd4aa04b5a424665c52f
shawnwjh <wong.shawnjh@gmail.com>
2024-03-20 14:03:32 +0800

    updated structure

---
+++ backend/ticket-management-system/src/main/java/com/masterticket/platform/ticketmanagementsystem/Controller/UserController.java
@@ -0,0 +1,30 @@
+package com.masterticket.platform.ticketmanagementsystem.Controller;
+
+import com.masterticket.platform.ticketmanagementsystem.Models.User;
+import com.masterticket.platform.ticketmanagementsystem.Models.responses.AuthResponse;
+import com.masterticket.platform.ticketmanagementsystem.Services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@CrossOrigin
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthResponse createUser(@RequestBody User user) {
+
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthResponse getUser(@RequestBody User user) {
+
+    }
+
+}
