c6e8cb69007f8b4ac047d92558e0a34887588bbe
shawnwjh <wong.shawnjh@gmail.com>
2024-03-24 18:01:12 +0800

    added ticketing officer creation

--- backend/ticket-management-system/src/main/java/com/masterticket/platform/ticketmanagementsystem/Repo/TicketingOfficerRepo.java
+++ backend/ticket-management-system/src/main/java/com/masterticket/platform/ticketmanagementsystem/Repo/TicketingOfficerRepo.java
@@ -1,9 +1,14 @@
 package com.masterticket.platform.ticketmanagementsystem.Repo;
 
+import com.masterticket.platform.ticketmanagementsystem.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.masterticket.platform.ticketmanagementsystem.Models.TicketingOfficer;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TicketingOfficerRepo extends JpaRepository<TicketingOfficer, Integer> {
-    
+    @Query(value = "SELECT * FROM user WHERE username = ?1 ", nativeQuery = true)
+    Optional<TicketingOfficer> findByUsername(String username);
 }
