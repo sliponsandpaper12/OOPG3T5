727f61eee7b3f409e74aecbd97d8001929373faf
sliponsandpaper12 <135314240+sliponsandpaper12@users.noreply.github.com>
2024-03-30 02:22:05 +0800

    added EMcancel and checking if user is EM

--- backend/ticket-management-system/src/main/java/com/masterticket/platform/ticketmanagementsystem/Services/CancellationAndRefundService.java
+++ backend/ticket-management-system/src/main/java/com/masterticket/platform/ticketmanagementsystem/Services/CancellationAndRefundService.java
@@ -47,4 +47,38 @@ public class CancellationAndRefundService {
             eventCategoryRepo.save(category);
         });
     }
+
+    
+    private final EventService eventService;
+    private final UserRepo userRepo;
+
+    @Autowired
+    public EmCancellationAndRefundService(EventService eventService, BookingService bookingService, UserRepo userRepo) {
+        this.eventService = eventService;
+        this.bookingService = bookingService;
+        this.userRepo = userRepo;
+    }
+
+    @Transactional
+    public void emCancelAndRefundEvent(Integer eventId, Integer userId) {
+        if (!isEventManager(userId)) {
+            throw new RuntimeException("User is not authorized to cancel the event."); // Use an appropriate exception
+        }
+
+        
+        eventService.cancelEvent(eventId);
+
+        //refund
+        var eventInDb = eventRepo.getReferenceById(eventId);
+        for (Booking booking : eventInDb.getBookings()) {
+            bookingService.refundBooking(booking.getBookingID());
+        }
+    }
+
+
+    public boolean isEventManager(Integer userId) {
+        var user = userRepo.getReferenceById(userId);
+        return user.getType() == 'E'; // assume e is em
+    }
+
 }
