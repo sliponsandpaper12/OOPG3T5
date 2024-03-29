43b2decc070a1348f313d2a3f9be38e800f389fd
sliponsandpaper12 <135314240+sliponsandpaper12@users.noreply.github.com>
2024-03-29 21:52:17 +0800

    added bookingDTO

---
+++ backend/ticket-management-system/src/main/java/com/masterticket/platform/ticketmanagementsystem/Models/DTO/BookingDTO.java
@@ -0,0 +1,20 @@
+package com.masterticket.platform.ticketmanagementsystem.Models.DTO;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingRequestDTO {
+
+    private LocalDateTime timestamp;
+    private Integer eventId;
+    private Integer userId;
+    private double amountPaid;
+    private boolean isRefunded;
+}
